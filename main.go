f16b99b659c03babbaf19e2f5c6ed1afa3dd3931
Dmitriy Miroshnichenko <dmiroshnichenko@ptsecurity.com>
2018-02-05 21:28:46 +0300

    Add: - groups search - user attrs

--- main.go
+++ main.go
@@ -6,12 +6,78 @@ import (
 	"log"
 )
 
-func main(){
+type User struct {
+	username string
+	name     string
+	mail     string
+}
+
+func getUserAttributes(dn, baseDN string, link ldap.Conn) User {
+	var user User
+
+	filter := fmt.Sprintf("(distinguishedName=%s)", dn)
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		filter,
+		[]string{"sn", "givenName", "mail", "sAMAccountName"},
+		nil,
+	)
+
+	sr, err := link.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fullName := sr.Entries[0].GetAttributeValue("givenName") + " " + sr.Entries[0].GetAttributeValue("sn")
+
+	user.name = fullName
+	user.username = sr.Entries[0].GetAttributeValue("sAMAccountName")
+	user.mail = sr.Entries[0].GetAttributeValue("mail")
+
+	return user
+}
+
+func getUsersAD(groupName, baseDN string, link ldap.Conn) {
+
+	var userList []User
+
+	filter := fmt.Sprintf("(&(objectClass=group)(cn=%s))", groupName)
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		filter,
+		[]string{"sn", "givenName", "mail", "member"},
+		nil,
+	)
+
+	sr, err := link.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, login := range sr.Entries[0].GetAttributeValues("member"){
+		userList = append(userList, getUserAttributes(login, baseDN, link))
+	}
+	fmt.Println(userList)
+}
+func getUsersTC() {}
+
+func main() {
 	// No TLS, not recommended
 	l, err := ldap.Dial("tcp", "")
 	if err != nil {
-		log.Println("Couldn't connect to LDAP server")
+		log.Fatal(err)
 	}
+	defer l.Close()
 
 	err = l.Bind("dmiroshnichenko@ptsecurity.com", "")
 	if err != nil {
@@ -19,11 +85,19 @@ func main(){
 		log.Println(err)
 	}
 
+	group := "R.MPX.ISIM.Repos.All.Reader"
+
+	//group := "DevOps"
+	//filter := fmt.Sprintf("(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:=%s))", "*Zabbix*")
+	filter := fmt.Sprintf("(&(objectClass=user)(objectCategory=Person)(memberOf:1.2.840.113556.1.4.1941:=%s))", group)
+	//filter := fmt.Sprintf("(distinguishedName=%s)", "CN=Vasiliy Zvyagintsev,OU=Users,OU=Tmsk,DC=ptsecurity,DC=ru")
+	//filter := fmt.Sprintf("(sAMAccountName=%s)", dn)
+	//filter := fmt.Sprintf("(&(objectClass=group)(cn=%s))", group)
 	searchRequest := ldap.NewSearchRequest(
 		"dc=ptsecurity,dc=ru", // The base dn to search
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		"(&(objectClass=group)(cn=R.MPX.ISIM.Repos.All.Reader))", // The filter to apply
-		[]string{"dn", "cn", "member"},                    // A list attributes to retrieve
+		filter,                        // The filter to apply
+		[]string{"dn", "cn", "member", "sn", "givenName", "mail"}, // A list attributes to retrieve
 		nil,
 	)
 
@@ -32,10 +106,20 @@ func main(){
 		log.Fatal(err)
 	}
 
-	for _, entry := range sr.Entries { //[0].Attributes[1].Values {
+	for _, entry := range sr.Entries { // [0].Attributes[1].Values {
+		fmt.Println(entry.GetAttributeValues("member"))
+		fmt.Println(entry.GetAttributeValues("mail"))
+		fmt.Println(entry.GetAttributeValues("dn"))
+		fmt.Println(entry.GetAttributeValues("cn"))
+		fmt.Println(entry.GetAttributeValues("sn"))
 		fmt.Println(entry)
 		//fmt.Println(entry.GetAttributeValues("member")[0])
 		//fmt.Println(entry.Attributes[0])
 		//fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("member"))
 	}
+
+
+	//fmt.Println(getUserAttributes("dmiroshnichenko", "dc=ptsecurity,dc=ru", *l))
+	//getUsersAD("*Zabbix*", "dc=ptsecurity,dc=ru", *l)
+
 }
