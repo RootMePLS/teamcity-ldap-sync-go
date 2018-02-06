04d7b44307310cfb2694cc17c60d7c3ed800a1cc
Dmitriy Miroshnichenko <dmiroshnichenko@ptsecurity.com>
2018-02-06 20:47:32 +0300

    done with LDAP

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"gopkg.in/ldap.v2"
 	"log"
 	"flag"
+	"net/http"
+	"net"
 )
 
 type User struct {
@@ -13,10 +15,39 @@ type User struct {
 	mail     string
 }
 
-func getUserAttributes(dn, baseDN string, link ldap.Conn) User {
+func getUsersFromAD(groupName, baseDN string, link ldap.Conn) []User {
+
+	var userList []User
+
+	filter := fmt.Sprintf("(&(objectClass=user)(objectCategory=Person)(memberOf:1.2.840.113556.1.4.1941:=%s))", groupName)
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		filter,
+		[]string{},
+		nil,
+	)
+
+	sr, err := link.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range sr.Entries {
+		userList = append(userList, getUserAttributes(entry.DN, baseDN, link))
+	}
+
+	return userList
+}
+
+func getUserAttributes(userDN, baseDN string, link ldap.Conn) User {
 	var user User
 
-	filter := fmt.Sprintf("(distinguishedName=%s)", dn)
+	filter := fmt.Sprintf("(distinguishedName=%s)", userDN)
 	searchRequest := ldap.NewSearchRequest(
 		baseDN,
 		ldap.ScopeWholeSubtree,
@@ -43,9 +74,7 @@ func getUserAttributes(dn, baseDN string, link ldap.Conn) User {
 	return user
 }
 
-func getUsersAD(groupName, baseDN string, link ldap.Conn) {
-
-	var userList []User
+func getGroupDN(groupName, baseDN string, link ldap.Conn) string {
 
 	filter := fmt.Sprintf("(&(objectClass=group)(cn=%s))", groupName)
 	searchRequest := ldap.NewSearchRequest(
@@ -56,7 +85,7 @@ func getUsersAD(groupName, baseDN string, link ldap.Conn) {
 		0,
 		false,
 		filter,
-		[]string{"sn", "givenName", "mail", "member"},
+		[]string{},
 		nil,
 	)
 
@@ -65,15 +94,32 @@ func getUsersAD(groupName, baseDN string, link ldap.Conn) {
 		log.Fatal(err)
 	}
 
-	for _, login := range sr.Entries[0].GetAttributeValues("member"){
-		userList = append(userList, getUserAttributes(login, baseDN, link))
-	}
-	fmt.Println(userList)
+	return sr.Entries[0].DN
 }
-func getUsersTC() {}
+
+func getTCGroups(groupName string){
+
+	client := &http.Client{}
+
+	url := "https://teamcity.ptsecurity.ru/app/rest/userGroups"
+	searcherReq, err := http.NewRequest("GET",url, nil)
+	searcherReq.Header.Add("Content-type", "application/json")
+	searcherReq.Header.Add("Accept", "application/json")
+
+
+	resp, err := client.Do(searcherReq)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+     //   resp = self.session.get(url, verify=False)
+	//resp.raise_for_status()
+	//groups_in_tc = resp.json()
+	//return [group for group in groups_in_tc['group']]
+}
 
 func main() {
-
 
 	username := flag.String("username", "username@domain.com", "Domain login for auth")
 	password := flag.String("password", "topSecret", "Password for auth")
@@ -91,45 +137,47 @@ func main() {
 
 	err = l.Bind(*username, *password)
 	if err != nil {
-		// error in ldap bind
 		log.Println(err)
 	}
 
-	group := "R.MPX.ISIM.Repos.All.Reader"
-
-	//group := "DevOps"
-	//filter := fmt.Sprintf("(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:=%s))", "*Zabbix*")
+	////group := "R.MPX.ISIM.Repos.All.Reader"
+	//group := "CN=R.MPX.ISIM.Repos.All.Reader,OU=DevOpsRoles,OU=Groups,OU=Msk,DC=ptsecurity,DC=ru"
+	////group := "DevOps"
+	////filter := fmt.Sprintf("(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:=%s))", "*Zabbix*")
 	//filter := fmt.Sprintf("(&(objectClass=user)(objectCategory=Person)(memberOf:1.2.840.113556.1.4.1941:=%s))", group)
-	//filter := fmt.Sprintf("(distinguishedName=%s)", "CN=Vasiliy Zvyagintsev,OU=Users,OU=Tmsk,DC=ptsecurity,DC=ru")
-	//filter := fmt.Sprintf("(sAMAccountName=%s)", dn)
-	filter := fmt.Sprintf("(&(objectClass=group)(cn=%s))", group)
-	searchRequest := ldap.NewSearchRequest(
-		"dc=ptsecurity,dc=ru", // The base dn to search
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		filter,                        // The filter to apply
-		[]string{"dn", "cn", "member", "sn", "givenName", "mail"}, // A list attributes to retrieve
-		nil,
-	)
+	////filter := fmt.Sprintf("(distinguishedName=%s)", "CN=Vasiliy Zvyagintsev,OU=Users,OU=Tmsk,DC=ptsecurity,DC=ru")
+	////filter := fmt.Sprintf("(sAMAccountName=%s)", dn)
+	////filter := fmt.Sprintf("(&(objectClass=group)(cn=%s))", group)
+	//searchRequest := ldap.NewSearchRequest(
+	//	"dc=ptsecurity,dc=ru", // The base dn to search
+	//	ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+	//	filter,                        // The filter to apply
+	//	[]string{"dn", "cn", "member", "sn", "givenName", "mail"}, // A list attributes to retrieve
+	//	nil,
+	//)
+	//
+	//sr, err := l.Search(searchRequest)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//for _, entry := range sr.Entries { // [0].Attributes[1].Values {
+	//	//fmt.Println(entry.GetAttributeValues("member"))
+	//	//fmt.Println(entry.GetAttributeValues("mail"))
+	//	//fmt.Println(entry.GetAttributeValues("dn"))
+	//	//fmt.Println(entry.GetAttributeValues("cn"))
+	//	//fmt.Println(entry.GetAttributeValues("sn"))
+	//	fmt.Println(entry.DN)
+	//	//fmt.Println(entry.GetAttributeValues("member")[0])
+	//	//fmt.Println(entry.Attributes[0])
+	//	//fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("member"))
+	//}
 
-	sr, err := l.Search(searchRequest)
-	if err != nil {
-		log.Fatal(err)
+	groups := []string{"R.MPX.ISIM.Repos.All.Reader",}
+	for _, groupName := range groups {
+		groupDN := getGroupDN(groupName, "dc=ptsecurity,dc=ru", *l)
+		users := getUsersFromAD(groupDN, "dc=ptsecurity,dc=ru", *l)
+		fmt.Println(users, "\n" ,len(users))
 	}
-
-	for _, entry := range sr.Entries { // [0].Attributes[1].Values {
-		fmt.Println(entry.GetAttributeValues("member"))
-		fmt.Println(entry.GetAttributeValues("mail"))
-		fmt.Println(entry.GetAttributeValues("dn"))
-		fmt.Println(entry.GetAttributeValues("cn"))
-		fmt.Println(entry.GetAttributeValues("sn"))
-		fmt.Println(entry)
-		//fmt.Println(entry.GetAttributeValues("member")[0])
-		//fmt.Println(entry.Attributes[0])
-		//fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("member"))
-	}
-
-
-	//fmt.Println(getUserAttributes("dmiroshnichenko", "dc=ptsecurity,dc=ru", *l))
-	//getUsersAD("*Zabbix*", "dc=ptsecurity,dc=ru", *l)
 
 }
